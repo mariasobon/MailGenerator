1ba79454806a255ff6e370329631def86cbbf522
Richart Raby <ryszard.raby@ceneo.pl>
2018-07-13 14:08:36 +0200

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -50,6 +50,13 @@ namespace MailGenerator.Controllers
             return View();
         }
 
+        public IActionResult Konto()
+        {
+            ViewBag.CustomFont = "font-family:'Segoe UI', Roboto, Helvetica, Arial, sans-serif";
+            ViewBag.Title = "Example Mail";
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
