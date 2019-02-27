da374315f80a687d78b8d6d373053f6b8082a673
ryszard_raby <ryszard.raby@gmail.com>
2019-02-27 15:16:43 +0100

    projects

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -74,6 +74,12 @@ namespace MailGenerator.Controllers
             ViewBag.Title = "Example Mail";
             return View();
         }
+        public IActionResult RemarketingBasket()
+        {
+            ViewBag.CustomFont = "font-family:'Segoe UI', Roboto, Helvetica, Arial, sans-serif";
+            ViewBag.Title = "Example Mail";
+            return View();
+        }
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
